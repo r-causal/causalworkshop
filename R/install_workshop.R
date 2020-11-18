166f442322a320a56d3d9596a1bd345ee3f9e921
Malcolm Barrett <malcolmbarrett@gmail.com>
2020-11-18 13:52:11 -0800

    update location and docs

--- R/install_workshop.R
+++ R/install_workshop.R
@@ -4,13 +4,15 @@
 #' Causal Inference in R on your computer. Then, it will open a new RStudio
 #' Project containing the files you'll need.
 #'
-#' @param path The path on your computer where you would like the workshop
-#'   installed
+#' @param destdir The path on your computer where you would like the workshop
+#'   installed. By default, this will install somewhere conspicous, like your
+#'   desktop, although you can tell `install_workshop()` exactly where you want
+#'   it to download.
 #'
 #' @export
-install_workshop <- function(path = ".") {
+install_workshop <- function(destdir = getOption("usethis.destdir")) {
   usethis::use_course(
-    "LucyMcGowan/user2020-causal-inference",
-    normalizePath(path)
+    "malcolmbarrett/causal_inference_r_workshop",
+    destdir = destdir
   )
 }
