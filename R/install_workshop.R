7103ae0917ca9b2963f54dff6338cda8728ed94a
Malcolm Barrett <malcolmbarrett@gmail.com>
2021-09-03 12:42:48 -0700

    fix typo :o

--- R/install_workshop.R
+++ R/install_workshop.R
@@ -5,7 +5,7 @@
 #' Project containing the files you'll need.
 #'
 #' @param destdir The path on your computer where you would like the workshop
-#'   installed. By default, this will install somewhere conspicous, like your
+#'   installed. By default, this will install somewhere conspicuous, like your
 #'   desktop, although you can tell `install_workshop()` exactly where you want
 #'   it to download.
 #'
